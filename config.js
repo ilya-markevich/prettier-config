7205e041d20728ece448542f1803e48e1a987c7e
Ilya Markevich <ilya.markevich1991@gmail.com>
2020-10-18 22:42:56 +0300

    add prettier config. (#1)

---
+++ config.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: false,
+  trailingComma: 'none',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'always',
+  rangeStart: 0,
+  rangeEnd: Infinity,
+  proseWrap: 'preserve',
+  htmlWhitespaceSensitivity: 'css',
+  endOfLine: 'lf',
+  embeddedLanguageFormatting: 'auto'
+};
